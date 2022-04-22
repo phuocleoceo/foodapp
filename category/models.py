58bc8c0327e137b88072e8c3dd1a758db7f926e7
phuocleoceo <ht10082001@gmail.com>
2022-04-22 21:26:47 +0700

    SuperUser

--- category/models.py
+++ category/models.py
@@ -7,5 +7,10 @@ class Category(models.Model):
     description = models.CharField(max_length=500, blank=True)
     image = models.ImageField(upload_to="photos/categories", blank=True)
 
+    # Hiển thị trong admin site, số ít và số nhiều
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.name
