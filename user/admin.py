c97734a93de8b5d509715d93bf6ebe8399a4eb0b
phuocleoceo <ht10082001@gmail.com>
2022-04-23 10:07:55 +0700

    Custom Superuser

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,21 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import User
 
+
+class AdminUser(UserAdmin):
+    # Các trường cho phép hiển thị
+    list_display = ("username", "email", "first_name", "last_name", "date_joined", "last_login", "is_active")
+    # Trường cho phép click để truy cập vào thông tin user
+    list_display_links = ("email", "first_name", "last_name")
+    # Trường chỉ cho phép đọc
+    readonly_fields = ("date_joined", "last_login")
+    # Sắp xếp theo
+    ordering = ("date_joined",)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, AdminUser)
