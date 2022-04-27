4ecda80294dba02c63b3ee136c74600d2107f0cc
phuocleoceo <ht10082001@gmail.com>
2022-04-27 22:49:11 +0700

    MediaFile

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 #     "ecommerce/static",
 # ]
 
+# Cấu hình đường dẫn Media file (ảnh,...)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
