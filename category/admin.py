d1a7bc38c1e2bdb415a5c6622708cb165ab25503
phuocleoceo <ht10082001@gmail.com>
2022-04-27 23:41:09 +0700

    Automatic Slug

--- category/admin.py
+++ category/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Category
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Sử dụng name làm khuôn cho slug
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name",)
+
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    slug = models.CharField(max_length=50, unique=True)
+    # allow_unicode để hỗ trợ UTF8
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
     description = models.CharField(max_length=500, blank=True)
     image = models.ImageField(upload_to="photos/categories", blank=True)
 
