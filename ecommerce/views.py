2348fc93a973e4f05a95d668ed78c30fac073bc8
phuocleoceo <ht10082001@gmail.com>
2022-05-09 00:09:56 +0700

    Display Product in HomePage

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from product.models import Product
 
 
 def HomePage(request):
-    return render(request=request, template_name="home.html")
+    products = Product.objects.all().filter(is_available=True)
+    return render(request=request, template_name="home.html", context={"products": products})
