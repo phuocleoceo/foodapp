1d98d5b524dc9d63659582b3b89c1a2ba1cdbba6
phuocleoceo <ht10082001@gmail.com>
2022-05-09 15:07:03 +0700

    Filter Product By Category Slug

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.product, name="product")
+    path("", views.product, name="product"),
+    path("<slug:category_slug>", views.product, name="product_by_category")
 ]
--- product/views.py
+++ product/views.py
@@ -1,12 +1,21 @@
+from django.shortcuts import render, get_object_or_404
 from category.models import Category
-from django.shortcuts import render
 from .models import Product
 
 
 # Create your views here.
-def product(request):
-    products = Product.objects.all().filter(is_available=True)
+def product(request, category_slug=None):
+    if category_slug != None:
+        # Lấy ra category cần filter từ category_slug
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=category, is_available=True)
+    else:
+        # Không dùng category để filter
+        products = Product.objects.all().filter(is_available=True)
+
+    # Đếm số lượng sản phẩm thu được
     quantity = products.count()
+    # Các danh mục sản phẩm để hiển thị
     categories = Category.objects.all()
     return render(request=request, template_name="product/product.html",
                   context={"products": products, "quantity": quantity, "categories": categories})
