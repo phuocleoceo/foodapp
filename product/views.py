b5554b6d917bcd13ed94ac0bd1d2530e7266bfcc
phuocleoceo <ht10082001@gmail.com>
2022-05-09 11:20:30 +0700

    Product Menu Page

--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
+from category.models import Category
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def product(request):
+    products = Product.objects.all().filter(is_available=True)
+    quantity = products.count()
+    categories = Category.objects.all()
+    return render(request=request, template_name="product/product.html",
+                  context={"products": products, "quantity": quantity, "categories": categories})
