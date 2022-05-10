d353fb514d63f75bf51006c35b9d9efd9f386e90
phuocleoceo <ht10082001@gmail.com>
2022-05-10 21:18:59 +0700

    Cart Model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,27 @@
+from product.models import Product
 from django.db import models
 
+
 # Create your models here.
+class Cart(models.Model):
+    """
+    Giỏ hàng
+    """
+    cart_id = models.CharField(max_length=100, blank=True)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    """
+    Sản phẩm trong giỏ hàng
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
