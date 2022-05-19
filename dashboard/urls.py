ee3aa3f1a04efe2e84fedefb54b4d40a98f6105f
phuocleoceo <ht10082001@gmail.com>
2022-05-19 10:43:49 +0700

    dashboard urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,14 @@ from . import views
 
 urlpatterns = [
     path("", views.HomePage, name="HomePage"),
-    path("category", views.CategoryManage, name="CategoryManage"),
-    path("product", views.ProductManage, name="ProductManage"),
+
+    path("category", views.ReadCategory, name="ReadCategory"),
+    path("category/create", views.CreateCategory, name="CreateCategory"),
+    path("category/<int:id>/update", views.UpdateCategory, name="UpdateCategory"),
+    path("category<int:id>/delete", views.DeleteCategory, name="DeleteCategory"),
+
+    path("product", views.ReadProduct, name="ReadProduct"),
+    path("product/create", views.CreateProduct, name="CreateProduct"),
+    path("product/<int:id>/update", views.UpdateProduct, name="UpdateProduct"),
+    path("product<int:id>/delete", views.DeleteProduct, name="DeleteProduct"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,15 +7,41 @@ def HomePage(request):
     return render(request=request, template_name="dashboard/home.html")
 
 
-def CategoryManage(request):
+def ReadCategory(request):
     # Các danh mục sản phẩm để hiển thị
     categories = Category.objects.all()
     return render(request=request, template_name="dashboard/category.html",
                   context={"categories": categories})
 
 
-def ProductManage(request):
+def CreateCategory(request):
+    pass
+
+
+def UpdateCategory(request, id):
+    pass
+
+
+def DeleteCategory(request, id):
+    pass
+
+####################################################################################
+
+
+def ReadProduct(request):
     # Các sản phẩm để hiển thị, kể cả không available
     products = Product.objects.all().filter()
     return render(request=request, template_name="dashboard/product.html",
                   context={"products": products})
+
+
+def CreateProduct(request):
+    pass
+
+
+def UpdateProduct(request, id):
+    pass
+
+
+def DeleteProduct(request, id):
+    pass
