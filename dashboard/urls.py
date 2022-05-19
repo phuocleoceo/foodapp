0c1e7e44ad92c774c1b21438bbc2eca08f0700bd
phuocleoceo <ht10082001@gmail.com>
2022-05-19 10:32:06 +0700

    READ Caegory,Product

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.HomePage, name="HomePage"),
+    path("category", views.CategoryManage, name="CategoryManage"),
+    path("product", views.ProductManage, name="ProductManage"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,21 @@
+from category.models import Category
 from django.shortcuts import render
+from product.models import Product
 
 
 def HomePage(request):
     return render(request=request, template_name="dashboard/home.html")
+
+
+def CategoryManage(request):
+    # Các danh mục sản phẩm để hiển thị
+    categories = Category.objects.all()
+    return render(request=request, template_name="dashboard/category.html",
+                  context={"categories": categories})
+
+
+def ProductManage(request):
+    # Các sản phẩm để hiển thị, kể cả không available
+    products = Product.objects.all().filter()
+    return render(request=request, template_name="dashboard/product.html",
+                  context={"products": products})
